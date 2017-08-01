980098dd8ea89cdd17a74d58d173dae9debd2fec
Brayden Langley <brayden@braydenlangley.com>
2017-07-31 21:36:00 -0700

    Worked on implementing the game setup page.

--- TickTockGo/GameSetupVC.cs
+++ TickTockGo/GameSetupVC.cs
@@ -8,22 +8,116 @@ namespace TickTockGo
     {
         public int NumberOfPlayers { get; set; }
         public string Variation { get; set; }
+        string _opponentSkillLevel;
+
+        UIButton selectedLevel;
 
         protected GameSetupVC(IntPtr handle) : base(handle)
         {
 			// Note: this .ctor should not contain any initialization logic.
 		}
 
+
+        /// <summary>
+        /// When the view loads.
+        /// </summary>
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
-            // Perform any additional setup after loading the view, typically from a nib.
+			// Perform any additional setup after loading the view, typically from a nib.
+
+			EnableKeyboardHiding(playerName1);
+
+			if (playerName2 != null)
+                EnableKeyboardHiding(playerName2);
         }
 
-        public override void DidReceiveMemoryWarning()
+        /// <summary>
+        /// Enables the keyboard hiding feature.
+        /// </summary>
+        /// <param name="inputField">Input field.</param>
+        void EnableKeyboardHiding(UITextField inputField)
         {
-            base.DidReceiveMemoryWarning();
-            // Release any cached data, images, etc that aren't in use.
+			inputField.ShouldReturn += (textField) =>
+			{
+				textField.ResignFirstResponder();
+				return true;
+			};
+        }
+
+        /// <summary>
+        /// When some one begins making a touch this code
+        /// will hide the keyboard.
+        /// </summary>
+        /// <param name="touches">Touches.</param>
+        /// <param name="evt">Evt.</param>
+        public override void TouchesBegan(Foundation.NSSet touches, UIEvent evt)
+        {
+            base.TouchesBegan(touches, evt);
+
+            this.View.EndEditing(true);
+        }
+
+        partial void Easy(Foundation.NSObject sender)
+        {
+            _opponentSkillLevel = "Easy";
+            SelectTheButton(sender as UIButton);
+        }
+		partial void Medium(Foundation.NSObject sender)
+		{
+            _opponentSkillLevel = "Medium";
+            SelectTheButton(sender as UIButton);
+		}
+		partial void Hard(Foundation.NSObject sender)
+		{
+            _opponentSkillLevel = "Hard";
+            SelectTheButton(sender as UIButton);
+		}
+
+        void SelectTheButton(UIButton selectedLevel)
+        {
+            selectedLevel.Alpha = 1;
+
+            if (easyButton != selectedLevel)
+            {
+                easyButton.Alpha = 0.1f;
+            }
+            if (mediumButton != selectedLevel)
+            {
+                mediumButton.Alpha = 0.1f;
+            }
+            if (hardButton != selectedLevel)
+            {
+                hardButton.Alpha = 0.1f;
+			}
+        }
+
+        /// <summary>
+        /// Prepares for segue.
+        /// </summary>
+        /// <param name="segue">Segue.</param>
+        /// <param name="sender">Sender.</param>
+        public override void PrepareForSegue(UIStoryboardSegue segue, Foundation.NSObject sender)
+        {
+            base.PrepareForSegue(segue, sender);
+
+            var dv = segue.DestinationViewController as GamePlayVC;
+
+			if (dv != null)
+			{
+                dv.OpponentSkillLevel = _opponentSkillLevel;
+                dv.Variation = Variation; // Should this be refactored to use a member variable?
+
+                if (NumberOfPlayers == 1)
+                {
+					dv.Players = new string[] { playerName1.Text};
+				}
+                else
+                {
+					dv.Players = new string[] { playerName1.Text, playerName2.Text }; // Refactor this code. 
+				}
+
+			}
         }
     }
 }
--- TickTockGo/GameSetupVC.designer.cs
+++ TickTockGo/GameSetupVC.designer.cs
@@ -1,21 +1,67 @@
-﻿// WARNING
+// WARNING
 //
-// This file has been generated automatically by Xamarin Studio from the outlets and
-// actions declared in your storyboard file.
-// Manual changes to this file will not be maintained.
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
 //
 using Foundation;
-using System;
 using System.CodeDom.Compiler;
-using UIKit;
 
 namespace TickTockGo
 {
-    [Register("GameSetupVC")]
-    partial class GameSetupVC
-    {
-        void ReleaseDesignerOutlets()
-        {
-        }
-    }
+	[Register ("GameSetupVC")]
+	partial class GameSetupVC
+	{
+		[Outlet]
+		UIKit.UIButton easyButton { get; set; }
+
+		[Outlet]
+		UIKit.UIButton hardButton { get; set; }
+
+		[Outlet]
+		UIKit.UIButton mediumButton { get; set; }
+
+		[Outlet]
+		UIKit.UITextField playerName1 { get; set; }
+
+		[Outlet]
+		UIKit.UITextField playerName2 { get; set; }
+
+		[Action ("Easy:")]
+		partial void Easy (Foundation.NSObject sender);
+
+		[Action ("Hard:")]
+		partial void Hard (Foundation.NSObject sender);
+
+		[Action ("Medium:")]
+		partial void Medium (Foundation.NSObject sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (playerName1 != null) {
+				playerName1.Dispose ();
+				playerName1 = null;
+			}
+
+			if (playerName2 != null) {
+				playerName2.Dispose ();
+				playerName2 = null;
+			}
+
+			if (easyButton != null) {
+				easyButton.Dispose ();
+				easyButton = null;
+			}
+
+			if (mediumButton != null) {
+				mediumButton.Dispose ();
+				mediumButton = null;
+			}
+
+			if (hardButton != null) {
+				hardButton.Dispose ();
+				hardButton = null;
+			}
+		}
+	}
 }
