7a1c176c443f3f77f39089837493ad0da52f573c
Brayden Langley <brayden@braydenlangley.com>
2017-08-01 23:07:16 -0700

    Removed unnecessary code.

--- TickTockGo/HomeVC.cs
+++ TickTockGo/HomeVC.cs
@@ -6,16 +6,6 @@ namespace TickTockGo
 {
     public partial class HomeVC : UIViewController
     {
-        UIButton[] cells;
-
-		int[] grid =
-		{    0,0,0,
-			 0,0,0,
-			 0,0,0
-		};
-
-        AI computerAI;
-
         protected HomeVC(IntPtr handle) : base(handle)
         {
             // Note: this .ctor should not contain any initialization logic.
@@ -25,7 +15,7 @@ namespace TickTockGo
         {
             base.PrepareForSegue(segue, sender);
 
-            var dv = segue.DestinationViewController as VariationVC;
+            var dv = segue.DestinationViewController as GameSetupVC;
 
 			if (dv != null)
 			{
@@ -49,37 +39,6 @@ namespace TickTockGo
 			this.NavigationController.View.BackgroundColor = UIColor.Clear;
 			this.NavigationController.NavigationBar.BackgroundColor = UIColor.Clear;
 			this.NavigationController.NavigationBar.ShadowImage = new UIImage();
-        }
-
-   //     partial void claim(Foundation.NSObject sender)
-   //     {
-   //         UIButton cell = sender as UIButton;
-   //         string id = cell.Tag.ToString();
-
-   //         computerAI.Set(Int32.Parse(id), false);
-   //         computerAI.AIturn(grid, 0, true, true);
-   //     }
-
-   //     partial void Reset(Foundation.NSObject sender)
-   //     {
-			//grid = new int[]
-    		//{    0,0,0,
-    		//	 0,0,0,
-    		//	 0,0,0
-    		//};
-
-        //    for (var x = 0; x < cells.Length; x++)
-        //    {
-        //        cells[x].SetImage(UIImage.FromBundle(""), UIControlState.Normal);
-        //    }
-
-        //    computerAI = new AI(cells, grid, this);
-        //}
-
-        public override void DidReceiveMemoryWarning()
-        {
-            base.DidReceiveMemoryWarning();
-            // Release any cached data, images, etc that aren't in use.
         }
     }
 }
